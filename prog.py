89f14dc079b358fef6bb1c5b87ef6b9968532cbb
unknown <hd_idrissou@esi.dz>
2021-01-09 17:33:37 +0100

    Update prog.py

--- prog.py
+++ prog.py
@@ -1,5 +1,6 @@
 # Une solution en programmation dynamique pour le probléme du Sac à Dos 
-def SacADos(W, wt, val, n): 
+
+def SelectObjets(W, Wi, Vi, n): 
     K = [[0 for x in range(W + 1)] for x in range(n + 1)] 
   
     # tableau K[][]  
@@ -7,8 +8,8 @@ def SacADos(W, wt, val, n):
         for w in range(W + 1): 
             if i == 0 or w == 0: 
                 K[i][w] = 0
-            elif wt[i-1] <= w: 
-                K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]],  K[i-1][w]) 
+            elif Wi[i-1] <= w: 
+                K[i][w] = max(Vi[i-1] + K[i-1][w-Wi[i-1]],  K[i-1][w]) 
             else: 
                 K[i][w] = K[i-1][w] 
     continu = True
@@ -20,17 +21,17 @@ def SacADos(W, wt, val, n):
         elif K[p-1][q]== K[p][q]:
             p-= 1
         else:
-            print("L'objet avec le poids = ",wt[p-1]," et le gain = ",val[p-1]," est ajouté au sac à dos")
-            q=q-wt[p-1]
+            print("L'objet avec le poids = ",Wi[p-1]," et le gain = ",Vi[p-1]," est ajouté au sac à dos")
+            q=q-Wi[p-1]
             p-= 1
     
     return K[n][W] 
   
 # Programme test  
-val = [60, 100, 120] 
-wt = [10, 20, 30] 
-W = 50
-n = len(val) 
-resultat = SacADos(W, wt, val, n)
+Vi = [1, 4, 5, 7] #tableau des gains Vi
+Wi = [1, 3, 4, 5] #tableau des poids Wi
+W = 7   
+n = len(Vi) 
+resultat = SelectObjets(W, Wi, Vi, n)
 print("Le gain maximal du sac à dos est :" , resultat) 
   
