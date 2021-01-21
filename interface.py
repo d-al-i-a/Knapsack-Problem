b8546199646113f25970d418f44b1ab3a8a60045
unknown <hd_idrissou@esi.dz>
2021-01-21 01:05:41 +0100

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,18 +1,20 @@
 # Une solution en programmation dynamique pour le probléme du Sac à Dos (avec interface graphique)
 from tkinter import *
-root = Tk()
+root = Tk(className=' TP1 TPRO - Problème Du Sac À Dos')
+root.geometry("1000x700")
+root.configure(background='#BEE5D3')
 #On récupère  le poid maximal et le nombre des objets 
-TitleLabel= Label(root,text="Solution du Problème du Sac à Dos ",font=("Times New Roman", 14) )
+TitleLabel= Label(root,text="Solution au Problème du Sac à Dos ",font=("Times New Roman", 14), bg='#BEE5D3' )
 TitleLabel.grid(row=0,column=1,pady=(20, 15),padx=(5,0))
-Title2Label= Label(root,text="(Programmation Dynamique) ",font=("Times New Roman", 14) )
+Title2Label= Label(root,text="(Programmation Dynamique) ",font=("Times New Roman", 14), bg='#BEE5D3'   )
 Title2Label.grid(row=0,column=2,pady=(20, 15))
 indice=1
-numberLabel= Label(root,text="Combien d'objets y a-t-il ? ",font=("Times New Roman", 12) )
+numberLabel= Label(root,text="Combien d'objets y a-t-il ? ",font=("Times New Roman", 12) , bg='#BEE5D3' )
 numberLabel.grid(row=indice,column=1,pady=(5, 5))
 numberEntry= Entry(root,width=40)
 numberEntry.grid(row=indice,column=2,pady=(5, 5))
 indice+=1
-maxPoidLabel= Label(root,text="Quel est le poids maximal du sac à dos ? ",font=("Times New Roman", 12) )
+maxPoidLabel= Label(root,text="Quel est le poids maximal du sac à dos ? ",font=("Times New Roman", 12), bg='#BEE5D3'  )
 maxPoidLabel.grid(row=indice,column=1,pady=(5, 5))
 maxPoidEntry= Entry(root,width=40)
 maxPoidEntry.grid(row=indice,column=2,pady=(5, 5))
@@ -25,13 +27,13 @@ def addElements():
     # On affiche un tableau à remplir avec les poids et gains des objets   
     for element in range(numberOfElement):
         elementString= "Poids de l'objet: "+ str(element)
-        elementLabel= Label(root,text=elementString,font=("Times New Roman", 12) )
+        elementLabel= Label(root,text=elementString,font=("Times New Roman", 12), bg='#BEE5D3'  )
         elementLabel.grid(row=element+4,column=1,pady=(5, 5))
         entriesPoids.append( Entry(root,width=30))
         entriesPoids[element].grid(row=element+4,column=2,pady=(5, 5))
         
         elementStringGain= "Gain de l'objet: "+ str(element)
-        elementLabelGain= Label(root,text=elementStringGain,font=("Times New Roman", 12) )
+        elementLabelGain= Label(root,text=elementStringGain,font=("Times New Roman", 12), bg='#BEE5D3'  )
         elementLabelGain.grid(row=element+4,column=3,pady=(5, 5))
         entriesGain.append(Entry(root,width=30))
         entriesGain[element].grid(row=element+4,column=4,pady=(5, 5))
@@ -74,21 +76,21 @@ def clickedElements():
                  p-= 1
             else:
                  objectstring= "L'objet avec le poids = "+ str(Wi[p-1])+ " et le gain = "+ str(Vi[p-1])+" est ajouté au sac à dos"
-                 objectlabel= Label(root,text=objectstring,font=("Times New Roman", 12) )
+                 objectlabel= Label(root,text=objectstring,font=("Times New Roman", 12) , bg='yellow')
                  objectlabel.grid(row=indiceRes,column=2)
                  q=q-Wi[p-1]
                  p-=1
             indiceRes+=1
         # On Affiche le gain maximal du sac à dos qui est la valeur du derniere cas du tableau créé 
         maxg=" Le gain maximal du sac à dos est : "+str( K[n][W])
-        maxlabel=Label(root,text=maxg,font=("Times New Roman", 14))
-        maxlabel.grid(row=indiceRes,column=3,pady=(5, 5))
+        maxlabel=Label(root,text=maxg,font=("Times New Roman", 14), bg='#BEE5D3', fg='red' )
+        maxlabel.grid(row=indiceRes,column=1,pady=(5, 5))
     # Button pour afficher le gain maximal du sac à dos et son contenu (objets selectionés)  
-    afficherGain= Button(root,text="Afficher le gain maximal",font=("Times New Roman", 10),command=clickMaxGain)
+    afficherGain= Button(root,text="Afficher le gain maximal",font=("Times New Roman", 12),command=clickMaxGain, borderwidth = '4', width = 25,bg='white' )
     afficherGain.grid(row=indice+n+2,column=2,pady=(5, 5))
 
  # Button pour afficher un tableau à remplir avec les poids et gains des objets   
-afficherElements= Button(root,text="Inserer les objects",font=("Times New Roman", 10),command=clickedElements)
+afficherElements= Button(root,text="Inserer les objects",font=("Times New Roman", 12),command=clickedElements, borderwidth = '4', width = 25,bg='white')
 afficherElements.grid(row=indice,column=2,pady=(5, 5))
 
 root.mainloop()
